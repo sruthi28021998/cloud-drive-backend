9bf208a54ab37b3d1692e83a5c5627fecd94b82c
sruthi28021998 <sruthibalabhadruni1998@gmail.com>
2026-08-22 15:45:56 +0530

    feat: add Postgres and Supabase client config

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected DB error', err);
+  process.exit(-1);
+});
+
+export const query = (text, params) => pool.query(text, params);
