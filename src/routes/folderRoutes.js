3be0a8cb6f8e06f79f2f2f2fee2ee8a6b8327f83
sruthi28021998 <sruthibalabhadruni1998@gmail.com>
2026-08-26 15:49:55 +0530

    Day 4: add folder and file CRUD APIs with hierarchy support

--- src/routes/folderRoutes.js
+++ src/routes/folderRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
-import { createFolder, getFolder, updateFolder, deleteFolder } from '../controllers/folderController.js';
+import { createFolder, getFolder, updateFolder, deleteFolder, getRoot } from '../controllers/folderController.js';
 
 const router = Router();
 
 router.use(requireAuth);
-router.post('/', createFolder);
+router.get('/root', getRoot);
+router.post('/', createFolder); 
 router.get('/:id', getFolder);
 router.patch('/:id', updateFolder);
 router.delete('/:id', deleteFolder);
